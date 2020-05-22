5a4a6b9d59e9388d289f555660b908bdf5cc7a3f
Alexander Gribochkin <pockomhadzzop@gmail.com>
2020-05-22 08:38:20 +0300

    docs(api): Added inline example

--- packages/api/index.d.ts
+++ packages/api/index.d.ts
@@ -3,6 +3,16 @@ export default class API extends Schema.Methods._domainsSpecifier {
     /**
      * @param {string} token Access token for VKontakte API
      * @param {string|number} version VKontakte API version, @default 5.103
+     *
+     * @example
+     *
+     * const api = new API(process.env.TOKEN);
+     *
+     * api.users.get({ user_ids: ["durov"] })
+     *   .then(users => users[0])
+     *   .then(founder => console.log(`${founder.first_name} ${founder.last_name}`))
+     *
+     * // Pavel Durov
      */
     constructor(token: string, v?: string | number);
 }
--- packages/api/index.js
+++ packages/api/index.js
@@ -77,6 +77,16 @@ class API extends Schema.Methods._domainsSpecifier {
     /**
      * @param {string} token Access token for VKontakte API
      * @param {string|number} version VKontakte API version, @default 5.103
+     *
+     * @example
+     *
+     * const api = new API(process.env.TOKEN);
+     *
+     * api.users.get({ user_ids: ["durov"] })
+     *   .then(users => users[0])
+     *   .then(founder => console.log(`${founder.first_name} ${founder.last_name}`))
+     *
+     * // Pavel Durov
      */
     constructor(token, v = 5.103) {
         super();
